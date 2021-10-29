f24df11d1cb66ce62a87e248f57e29bf5ba5aa71
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-29 16:30:53 +0100

    Refactored toggle for variables > classes

--- src/lib/Toggle.svelte
+++ src/lib/Toggle.svelte
@@ -1,33 +1,73 @@
 <script>
-	//We get the below 'name' and 'toggled' variables from wherever we use this component in.
-	export let name,
-		toggled = false;
-	$: responsiveToggled = toggled;
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 	const dispatch = createEventDispatcher();
-
-	//When button clicked, dispatch an event.
-	//We control what happens THERE, not here.
-	//Include the name of the dispatched 'toggle' incase you use multiple toggles and need to differentiate between them.
+	export let name,
+		toggledState = false,
+		colorType,
+		size;
+	let width, height, spanWidth, spanHeight, translateX, col, colActive;
+	
 	const dispatchToggle = () => {
-		dispatch('toggled', name);
+		dispatch('toggled', name); //'dmToggle'
 	};
+
+	const sizes = [
+		//Height = width * 0.513513514
+		{
+			size: 'normal',
+			width: 'w-[7.4rem]',
+			height: 'h-[3.8rem]',
+			translateX: 'translate-x-[3.6rem]',
+			spanWidth: 'w-[3.4rem]',
+			spanHeight: 'h-[3.4rem]'
+		},
+		{
+			size: 'small',
+			width: 'w-[5rem]',
+			height: 'h-[2.57rem]',
+			translateX: 'translate-x-[2.3rem]',
+			spanWidth: 'w-[2.15rem]',
+			spanHeight: 'h-[2.15rem]'
+		}
+	];
+
+	const colors = [
+		{
+			color: 'teal',
+			col: 'bg-teal-400',
+			colActive: 'bg-teal-700'
+		},
+		{
+			color: 'blue',
+			col: 'bg-blue-400',
+			colActive: 'bg-blue-700'
+		}
+	];
+
+	const updateVariables = () => {
+		width = sizes.find((s) => s.size === size).width;
+		height = sizes.find((s) => s.size === size).height;
+		spanWidth = sizes.find((s) => s.size === size).spanWidth;
+		spanHeight = sizes.find((s) => s.size === size).spanHeight;
+		translateX = sizes.find((s) => s.size === size).translateX;
+		col = colors.find((c) => c.color === colorType).col;
+		colActive = colors.find((c) => c.color === colorType).colActive;
+	};
+
+	onMount(() => updateVariables());
 </script>
 
 <button
-	class="w-[7.4rem] h-[3.8rem] rounded-full cursor-pointer border-2 border-transparent inline-flex
-	flex-shrink-0 transition-colors duration-200 ease-in-out {!responsiveToggled ? 'bg-teal-700' : 'bg-teal-900' }"
+	class="{width}
+	{height} rounded-full cursor-pointer border-2 border-transparent inline-flex flex-shrink-0
+	transition-colors duration-200 ease-in-out {!toggledState ? col : colActive}"
 	role="switch"
 	tabindex="0"
 	aria-checked="false"
 	on:click={dispatchToggle}>
 	<span
 		aria-hidden="true"
-		class="h-[3.4rem] w-[3.4rem] bg-white rounded-full transition inline-block duration-200
-		ease-in-out {!responsiveToggled ? 'translate-x-0' : 'translate-x-[3.6rem]'}"
-		/>
+		class="
+		{spanWidth}
+		{spanHeight} bg-white rounded-full transition inline-block duration-200 ease-in-out {!toggledState ? 'translate-x-0' : translateX}" />
 </button>
-
-<style>
-
-</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,23 +3,33 @@
 	import Toggle from '$lib/Toggle.svelte'
 	import {onMount} from 'svelte';
 	import DarkMode from '$lib/stores/darkMode.js' //This is where all the work happens
+	import PlainNavBar from '$lib/navigation/PlainNavBar.svelte';
 
 	$: dmToggleData = {
 		name: 'dmToggle',
-		toggled: $DarkMode
+		toggledState: $DarkMode,
+		size: 'normal',
+		colorType: 'teal'
 	}
-	
+
 	onMount(() => {
 		DarkMode.checkDarkmode()
 	})
 
 	const handleToggle = (e) => {
 		let name = e.detail
-		if(name === 'dmToggle'){
-			DarkMode.toggleDarkmode()
-		}
+		const handleDM = name === 'dmToggle' ? DarkMode.toggleDarkmode() : '';
 	}
 </script>
+
+<PlainNavBar>
+	<svg width="100%" height="100%" viewBox="0 0 222 222" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;" slot="logo">
+		<circle cx="110.872" cy="48.693" r="48.693"/>
+		<circle cx="172.974" cy="173.051" r="48.693"/>
+		<circle cx="48.693" cy="173.051" r="48.693"/>
+	</svg>
+</PlainNavBar>
+
 
 <slot />
 
