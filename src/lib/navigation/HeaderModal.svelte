30f46d534b8ed3dfd6442007b8fc74c3adf6c7c0
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-29 23:35:42 +0100

    created header component

---
+++ src/lib/navigation/HeaderModal.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fly } from 'svelte/transition';
+</script>
+
+<div
+	class="w-full h-[max-content] absolute top-[120%] left-0 shadow-lg ring-1 ring-black ring-opacity-5
+	rounded-lg sm:hidden px-10 py-10 dark:bg-gray-700 transition-colors"
+	transition:fly={{ y: 5 }}>
+	<slot />
+</div>
