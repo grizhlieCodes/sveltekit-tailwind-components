7bea10ea18b7b3661d857f9aa6573d1bf9c34775
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-29 23:30:33 +0100

    created icon comp

---
+++ src/lib/decoration/Icon.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let name, width = '2.4rem', focusable = false, extraStyling = '';
+    let icons = [
+        {
+            name: 'home',
+            box: '0 0 24 24',
+            svg: ` <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />`
+        },
+        {
+            name: 'about',
+            box: '0 0 24 24',
+            svg: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />`
+        },
+        {
+            name: 'contact',
+            box: '0 0 24 24',
+            svg: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />`
+        },
+        {
+            name: 'services',
+            box: '0 0 24 24',
+            svg: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />`
+        },
+        {
+            name: 'work',
+            box: '0 0 24 24',
+            svg: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />`
+        },
+    ]
+
+    let displayIcon = icons.find(i => i.name === name)
+</script>
+
+<svg class="{$$props.class}" viewBox="{displayIcon.box}" {width} {focusable} style="{extraStyling}" fill="none">
+    {@html displayIcon.svg}
+</svg>
+
+<style>
+    :global(svg path){
+        stroke-width: 1.5px;
+    }
+</style>
