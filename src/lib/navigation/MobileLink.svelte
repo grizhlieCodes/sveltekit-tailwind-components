55a2445af10ad9d4dc21dccf5f259822cc9e2f7f
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-29 23:35:51 +0100

    created mobile link

---
+++ src/lib/navigation/MobileLink.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Icon from '$lib/decoration/Icon.svelte'
+    export let link
+</script>
+
+<a href="{link.path}" class="w-full px-4 py-5 rounded-lg cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors flex flex-row items-center justify-start gap-5">
+						
+    <Icon name="{link.smallName}" width="2.4rem" class="stroke-current text-teal-500 dark:text-teal-400"/>
+    
+    <p class="font-sans text-[1.6rem] text-gray-800 dark:text-gray-300 transition-colors">{link.name}</p>			
+</a>
+
+<style>
+    
+</style>
