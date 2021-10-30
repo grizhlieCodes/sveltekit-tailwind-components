d82a1317c6cfdf436efc96453cfcb068228a7711
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-30 20:57:54 +0100

    added custom breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,23 @@ const config = {
 
 	theme: {
 		extend: {
+			transformOrigin: {
+				'1': '1px 1px',
+			},
 			colors: {
 				teal: colors.teal,
 				orange: colors.orange
 			},
+			screens: {
+				"phone": '375px',
+				"phone-wide": '480px',
+				"phablet": '560px',
+				"tablet-small": '640px',
+				"tablet": '768px',
+				"tablet-wide": '1024px',
+				"desktop": '1248px',
+				"desktop-wide": '1440px'
+			}
 		}
 	},
 
