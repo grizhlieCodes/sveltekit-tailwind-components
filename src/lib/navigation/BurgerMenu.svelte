b7242eec652ad749deaa71efeec622cac66459ee
grizhlieCodes <rafal.potasz@gmail.com>
2021-10-30 20:58:23 +0100

    updated burger menu origin styling

--- src/lib/navigation/BurgerMenu.svelte
+++ src/lib/navigation/BurgerMenu.svelte
@@ -12,15 +12,9 @@
 	cursor-pointer group sm:hidden"
 	on:click={dispatchClick}>
 	<span
-		class=" bg-gray-400 dark:bg-gray-300 w-full h-[0.2rem] block transition-all duration-200 {burgerMenuClicked ? 'rotate-45' : ''}" />
+		class=" bg-gray-400 dark:bg-gray-300 w-full h-[0.2rem] block transition-all duration-200 {burgerMenuClicked ? 'rotate-45' : ''} origin-[1px]" />
 	<span
-		class=" bg-gray-400 dark:bg-gray-300 w-full h-[0.2rem] block transition-all duration-200 {burgerMenuClicked ? ' scale-50 opacity-0' : ''}" />
+		class=" bg-gray-400 dark:bg-gray-300 w-full h-[0.2rem] block transition-all duration-200 {burgerMenuClicked ? ' scale-50 opacity-0' : ''} origin-[1px]" />
 	<span
-		class=" bg-gray-400 dark:bg-gray-300 w-full h-[0.2rem] block transition-all duration-200 {burgerMenuClicked ? '-rotate-45  ' : ''}" />
+		class=" bg-gray-400 dark:bg-gray-300 w-full h-[0.2rem] block transition-all duration-200 {burgerMenuClicked ? '-rotate-45  ' : ''} origin-[1px]" />
 </div>
-
-<style>
-	.burger-menu > span {
-		transform-origin: 1px 1px;
-	}
-</style>
